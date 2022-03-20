ccc25b25157020150ad0e8108a7383df3518f344
marisagn97 <malonsogn@gmail.com>
2022-03-20 12:18:27 +0100

    Avance descriptivo

--- mmm.R
+++ mmm.R
@@ -8,91 +8,99 @@ sales <- read_csv('data/mmm_sales.csv')
 macro <- read_csv('data/mmm_macro.csv')
 spend <- read_csv('data/mmm_spend.csv')
 compets <- read_csv('data/mmm_compets.csv')
-canibalizacion <- read_csv('data/mmm_canibalización.csv')
+canibalizacion <- read_csv('data/mmm_canib.csv')
 a=list(sales,macro,spend,compets,canibalizacion)
 df= Reduce(function(...) merge(..., all=T), a)
-df <- df%>% mutate(tv = rowSums(select(., starts_with("tv"))))
+df <- df%>%filter(fecha>'2019-01-01')
 write.csv(df, "datos.csv")
-
-#Veo si hay alguna fecha duplicada
-sum(duplicated(df$fecha))
-#Veo si hay na
-sum(is.na(df))
-#Veo fecha inicial y final
-min(df$fecha)
-max(df$fecha)
-#compruebo dim df para ver que no falten semanas
-dim(df)
 #Creo una columna con los años y otra con la semana
-df['year']=as.numeric(format(df$fecha,'%Y'))
-df['month']=month(df$fecha,label=TRUE)
-df['week']=week(df$fecha)
+df <- df %>% mutate(year=as.numeric(format(df$fecha,'%Y')),
+                    month=month(df$fecha,label=TRUE),
+                    week=week(df$fecha),
+                    tv_foo = rowSums(select(., starts_with("tv"))))
+df<- df %>%select(where(~ any(. != 0)))
+
 #Gráficas lineales (Series de tiempo)
 require(scales)
-G1 <- ggplot(data=df, aes(x=fecha, y=v_vol_foo_normal_format1)) +
+Gvol <- ggplot(data=df, aes(x=fecha, y=v_vol_foo_normal_format1)) +
   geom_line()+ scale_y_continuous(labels = comma)
-G1_1 <- ggplot(data=df, aes(x=week,y=v_vol_foo_normal_format1)) +
+Gvolyear <- ggplot(data=df, aes(x=week,y=v_vol_foo_normal_format1)) +
   geom_line()+ scale_y_continuous(labels = comma)+
   scale_x_continuous(breaks = seq(from = 1, to = 53, by =5 ))+
   facet_grid(year~.)
-G2 <- ggplot(data=df, aes(x=fecha, y=v_euros_foo_normal_format1)) +
-  geom_line()+ scale_y_continuous(labels = comma)
-G3 <- ggplot(data=df, aes(x=fecha, y=pbase_foo_normal_format1)) +
-  geom_line()
-grid.newpage()
-grid.draw(rbind(ggplotGrob(G1), ggplotGrob(G2), ggplotGrob(G3),size = "last"))
 
-G4 <- ggplot(data=df, aes(x=fecha, y=dp_promo_foo_normal_format1)) +
-  geom_line()
-G4_1 <- ggplot(data=df, aes(x=week,y=dp_promo_foo_normal_format1)) +
+Gvollog <- ggplot(data=df, aes(x=fecha, y=log(v_vol_foo_normal_format1))) +
+  geom_line()+ scale_y_continuous(labels = comma)
+Gvolyearlog <- ggplot(data=df, aes(x=week,y=log(v_vol_foo_normal_format1))) +
   geom_line()+ scale_y_continuous(labels = comma)+
   scale_x_continuous(breaks = seq(from = 1, to = 53, by =5 ))+
   facet_grid(year~.)
-G5 <- ggplot(data=df, aes(x=fecha, y=dp_foo_normal_format1)) +
+
+#Tiendas a las que esta expuesto
+Gexpuesto<- ggplot(data=df, aes(x=fecha, y=dp_foo_normal_format1)) +
   geom_line()
-G5_1 <-ggplot(data=df, aes(x=week,y=dp_foo_normal_format1)) +
+Gexpuestoyear<-ggplot(data=df, aes(x=week,y=dp_foo_normal_format1)) +
   geom_line()+ scale_y_continuous(labels = comma)+
   scale_x_continuous(breaks = seq(from = 1, to = 53, by =5 ))+
   facet_grid(year~.)
 min(df$dp_foo_normal_format1)#para saber el min al que esta expuesto
+#la exposicion da un bajon a finales de 2021
 
-G6 <- ggplot(data=df, aes(x=fecha)) +
-  geom_line(aes(y=facebook_foo),color='blue')+
-  geom_line(aes(y=twitter_foo),color='orange')+
-  geom_line(aes(y=web_foo),color='green')+
-  geom_line(aes(y=you_tube_foo),color='red')+
-  scale_y_continuous(labels = comma)
-
-G6_1 <- ggplot(data=df, aes(x=week)) +
-  geom_line(aes(y=facebook_foo),color='blue')+
-  geom_line(aes(y=twitter_foo),color='orange')+
-  geom_line(aes(y=web_foo),color='green')+
-  geom_line(aes(y=you_tube_foo),color='red')+
-  scale_y_continuous(labels = comma)+
-  scale_x_continuous(breaks = seq(from = 1, to = 53, by =5 ))+
-  facet_grid(year~.)
-
-G7 <- ggplot(data=df, aes(x=fecha, y=tv))+
+#Grafica TV
+GTV<- ggplot(data=df, aes(x=fecha, y=tv_foo))+
   geom_line()
-G7_1 <-ggplot(data=df, aes(x=week, y=tv))+
+GTVyear<-ggplot(data=df, aes(x=week, y=tv_foo))+
   geom_line()+
   scale_x_continuous(breaks = seq(from = 1, to = 53, by =5 ))+
   facet_grid(year~.)
 
+#Grafico TV con vol ventas
+grid.newpage()
+grid.draw(cbind(ggplotGrob(GTVyear), ggplotGrob(Gvolyear),size = "last"))
 
-#Vamos a ver que dias coinciden campañas y cuantas
-Campaign <- df%>%select(starts_with("tv"))
-ValTV <- as.data.frame(apply(Campaign[,2:13], 2, table))
-MeanTV <- apply(Campaign[,2:13], 2, mean)
-Campaign[Campaign !=0.0] <-1
-fecha=df$fecha
-Campaign <- cbind(fecha,Campaign)
-Campaign<- Campaign%>% mutate(Coinciden= rowSums(Campaign[,c (2:12)], na.rm = TRUE))
-Coincidencias <- Campaign %>% select(fecha,Coinciden) %>%  filter(Coinciden>1)
-#Coinciden campañas en 84 ocasiones
+#Correlaciones
+newspend <- spend %>%filter(fecha>'2019-01-01')%>%select(where(~ any(. != 0))) %>% colnames()
+x1 <- df[,newspend]
+x1 <- x1[,2:length(x1)]
+x2 <- df[,c('dp_foo_normal_format1','v_euros_foo_normal_format1','pbase_foo_normal_format1', 'dif_temp', 'precip', 'laboralidad', 'tv_foo')]
+x <- cbind(x1,x2)
+y <- df[,c('v_vol_foo_normal_format1')]
+corr <- cor(x,y)
+corr[order(abs(corr))]
 
-#Voy a calcular la estacionalidad del vol
-v_vol_foo_normal_format1.ts = ts(df$v_vol_foo_normal_format1, start = c(2011,8), frequency = 52)
-boxplot(v_vol_foo_normal_format1.ts ~ cycle(v_vol_foo_normal_format1.ts))
+#Estacionalidad
+v_vol_foo_normal_format1.ts = ts(df$v_vol_foo_normal_format1, start = c(2019,1), frequency = 52)
 v_vol_foo_normal_format1.ts.desc = decompose(v_vol_foo_normal_format1.ts)
 plot(v_vol_foo_normal_format1.ts.desc)
+
+#Estacionalidad log
+v_vol_foo_normal_format1.ts = ts(log(df$v_vol_foo_normal_format1), start = c(2019,1), frequency = 52)
+v_vol_foo_normal_format1.ts.desc = decompose(v_vol_foo_normal_format1.ts)
+plot(v_vol_foo_normal_format1.ts.desc)
+
+#Sin log TV
+v <- df$tv_foo
+m <- max(v)
+a <- seq(0.5,1,0.1)*m
+Tvmod <- matrix(data=NA, nrow=length(v), ncol=length(a))
+for (j in (1:length(a))){
+  Tvmod[ , j] <- v/(v+a[j])
+}
+#Tvmod <- as.data.frame(Tvmod)
+p <- seq(0.4,0.9,0.1)
+len <- length(p)
+list <- vector(mode = "list", length = len)
+for (j in (1:len)){
+  list[[j]]=Tvmod*p[j]
+}
+Mtv=cbind(list[[1]],list[[2]],list[[3]],list[[4]],list[[5]],list[[6]])
+corrTV <- cor(Mtv,y)
+which.max(corrTV) #esto implica 2a,3p, es decir
+a[2]
+p[3]
+
+#logTV
+corrTV2 <- cor(Mtv,log(y))
+which.max(corrTV2) #esto implica 1a,1p, es decir
+a[1]
+p[1]
